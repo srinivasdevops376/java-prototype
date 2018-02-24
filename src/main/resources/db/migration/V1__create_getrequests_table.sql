7ce270421e53858fffaaeb69e973170c291361ad
Bill Koch <koch.149@osu.edu>
2018-02-23 22:24:18 -0500

    Adding Flyway to handle database schema creation and migrations
    Signed-off-by: Bill Koch <koch.149@osu.edu>

---
+++ src/main/resources/db/migration/V1__create_getrequests_table.sql
@@ -0,0 +1,10 @@
+
+GRANT delete, insert, select, update ON java_prototype.* TO ${application_username};
+
+-- Create syntax for 'java_prototype.getrequests'
+CREATE TABLE getrequests (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  timestamp timestamp NULL DEFAULT NULL,
+  hash varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=499 DEFAULT CHARSET=utf8;
